1af2bb4475faaa2f68d09bb0f53fae74eee6e8b8
kcyesilyurt <s239446497@stu.thk.edu.tr>
2026-04-04 20:18:44 +0300

    Added debug.log to see what is the problem?

--- Assets/MissileLauncher.cs
+++ Assets/MissileLauncher.cs
@@ -10,14 +10,26 @@ public class MissileLauncher : MonoBehaviour
 
     public GameObject Launch(Transform target)
     {
-        if (missilePrefab == null || launchPoint == null) return null;
+        Debug.Log("Launch called");
+        if(missilePrefab == null)
+        {
+            Debug.Log("missileprefab is null");
+            return null;
+        }
+        if (launchPoint == null)
+        {
+            Debug.Log("launchpoint is null");
+            return null;
+        }
 
         activeMissile = Instantiate(missilePrefab, launchPoint.position, launchPoint.rotation);
+        Debug.Log("Missile instantiated: " + activeMissile.name);
 
         MissileHoming homing = activeMissile.GetComponent<MissileHoming>();
         if (homing != null)
         {
             homing.SetTarget(target);
+            Debug.Log("Target assigned");
         }
 
         if (launchAudioSource != null)
